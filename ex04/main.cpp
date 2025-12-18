8a0d343f9d6ba7eb6955d89479b79c5c7a73cc0b
kaisuzuk <szkakg@gmail.com>
2025-12-18 09:15:34 +0900

    ex04終了

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -6,7 +6,7 @@
 /*   By: kaisuzuk <kaisuzuk@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/17 12:21:25 by kaisuzuk          #+#    #+#             */
-/*   Updated: 2025/12/17 13:25:58 by kaisuzuk         ###   ########.fr       */
+/*   Updated: 2025/12/18 09:07:47 by kaisuzuk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,21 +27,49 @@ void printFileError(const std::string &filename) {
     std::cout << filename << " cannot open. " << std::endl;
 }
 
-bool readAll(const std::string &filename, std::string &out) {
+bool readFile(const std::string &filename, std::string &fileText) {
     std::ifstream file(filename);
     if (!file)
     {
         printFileError(filename);
         return (false);
     }
-    out.assign(std::istreambuf_iterator<char>(file), std::istreambuf_iterator<char>());
+    fileText.assign(std::istreambuf_iterator<char>(file), std::istreambuf_iterator<char>());
     return (true);
 }
 
-void replaceAll()
+std::string replaceAll(const std::string &src, const std::string &s1, const std::string &s2) {
+    std::string result;
+    std::string::size_type pos;
+    std::string::size_type found;
+
+    pos = 0;
+    while ((found = src.find(s1, pos)) != std::string::npos)
+    {
+        result.append(src, pos, found - pos);
+        result += s2;
+        pos = found + s1.length();
+    }
+    result.append(src, pos, std::string::npos);
+    return (result);
+}
+
+bool writeFile(const std::string &filename, const std::string &data) {
+    std::string outName;
+
+    outName = filename + ".replace";
+    std::ofstream ofs(outName.c_str());
+    if (!ofs)
+    {
+        printFileError(outName);
+        return (false);
+    }
+    ofs << data;
+    return (true);
+}
 
 int main(int argc, char *argv[]) {
-    std::string out;
+    std::string fileText;
     std::string filename;
     std::string s1;
     std::string s2;
@@ -59,6 +87,9 @@ int main(int argc, char *argv[]) {
         printValid();
         return (0);
     }
-
-
+    if (!readFile(filename, fileText))
+        return (2);
+    if (!writeFile(filename, replaceAll(fileText, s1, s2)))
+        return (2);
+    return (0);
 }
